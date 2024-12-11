874d0930ba5fbba903045e5d07600cdd9605c05a
asadbek <asadbekqulboyev@gmail.com>
2024-12-11 07:49:58 +0500

    Finish

--- modules/fs.js
+++ modules/fs.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-fs.mkdir(path.join(__dirname, 'templates','december.txt'), err => {
-    if (err) throw new Error()
-    console.log('Error')
-}
-)
+// fs.mkdir(path.join(__dirname, 'notes','december.txt'), (err) => {
+//     if (err) throw new Error()
+//     console.log('Error')
+// }
+// )
+// fs.mkdir(path.join(__dirname,'notes'),error=>{
+//     if(error) throw new Error()
+//     console.log('Folder was create succesfull');
+// })
+fs.writeFile(path.join(__dirname, 'notes','december.txt'),'Node jsni organyapman',(err)=>{
+    if(err) throw new Error()
+        console.log('New successfull');
+        
+        
+})
