ed50ca86d79d02a7709c7e83c9fa2939e8b09627
asadbek <asadbekqulboyev@gmail.com>
2024-12-16 13:51:31 +0500

    Os end

--- modules/fs.js
+++ modules/fs.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os')
 // fs.mkdir(path.join(__dirname, 'notes','december.txt'), (err) => {
 //     if (err) throw new Error()
 //     console.log('Error')
@@ -9,9 +10,25 @@ const path = require('path');
 //     if(error) throw new Error()
 //     console.log('Folder was create succesfull');
 // })
-fs.writeFile(path.join(__dirname, 'notes','december.txt'),'Node jsni organyapman',(err)=>{
-    if(err) throw new Error()
-        console.log('New successfull');
+// fs.writeFile(path.join(__dirname, 'notes','december.txt'),'Node jsni organyapman',(err)=>{
+//     if(err) throw new Error()
+//         console.log('New successfull');
         
         
-})
+// })
+// fs.appendFile(path.join(__dirname,'notes','december.txt'),' keyingi text', (err)=>{
+//     if (err) throw new Error()
+// })
+// fs.readFile(path.join(__dirname,'notes','december.txt'), (err,data)=>{
+//     if (err) throw new Error()
+
+//         console.log(Buffer.from(data).toString().split('\n'));
+        
+// })
+// console.log(os.arch()); - razryadi
+// console.log(os.platform()); - operatsion sistemani bildiradi
+// console.log(os.cpus());
+// console.log(os.freemem()); - bo'sh joy
+// console.log(os.totalmem()/(1024 * 1024).toFixed(2));
+// console.log(os.freemem()/(1024 * 1024).toFixed(2));
+console.log(os.homedir());
