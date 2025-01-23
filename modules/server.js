62f6747149781bb96b921ba537ecfbf9b53ca7e1
asadbek <asadbekqulboyev@gmail.com>
2025-01-23 10:31:48 +0500

    Fayllar bilan ishlash

--- modules/server.js
+++ modules/server.js
@@ -1,30 +1,40 @@
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+// console.log(fs.readFile(path));
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.end(`
-            <h1>Send Name</h1> 
-            <form method='post' action='/'>
-                <input name="name" placeholder='Ismingizni kiriting'/>
-                <button type='submit'>Send Name</button>
-            </form>
-        `);
-    } else if (req.method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk); // Kichik qismlarni yig'amiz
-        });
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'GET') {
+//         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    
+//         if(req.url ==='/'){
+//             fs.readFile(path.join(__dirname,'templates','index.html'),'utf-8',(err,conten)=>{
+//                 if(err) throw err
+//                 res.end(conten)
+//             })
+//         }else if(req.url === '/about'){
+//             fs.readFile(path.join(__dirname,'templates','about.html'),'utf-8',(err,conten)=>{
+//                 if(err) throw err
+//                 res.end(conten)
+//             })
+//         }else if(req.url === '/contact'){
+//             fs.readFile(path.join(__dirname,'templates','contact.html'),'utf-8',(err,conten)=>{
+//                 if(err) throw err
+//                 res.end(conten)
+//             })
+//         }
+//     } else if (req.method === 'POST') {
+//         const body = [];
+//         req.on('data', (chunk) => {
+//         body.push(chunk)})
+//         req.on('end', () => {
+//         const parsedBody = Buffer.concat(body).toString();
+//         const message = decodeURIComponent(parsedBody.split('=')[1]); 
+//         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//         res.end(`Name successfully added: ${message}`)});
+//     }
+// });
 
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString(); // To'liq ma'lumotni yig'ish
-            const message = decodeURIComponent(parsedBody.split('=')[1]); // Form ma'lumotini ajratish
-            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.end(`Name successfully added: ${message}`); // Javob berish
-        });
-    }
-});
-
-server.listen(3000, () => {
-    console.log('Server has started on port 3000');
-});
+// server.listen(3000, () => {
+//     console.log('Server has started on port 3000');
+// });
